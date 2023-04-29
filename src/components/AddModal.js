8f686158699af55ca0f8009c42567031e46fe41e
cvachha <cvachha549@gmail.com>
2023-04-28 21:41:49 -0700

    Fixed issue with adding new item with in stock unchecked
    Fixed issue where adding an out of stock item stopped working. I made the inStock variable a true/false boolean

--- src/components/AddModal.js
+++ src/components/AddModal.js
@@ -7,125 +7,125 @@ import { db } from '../firebase/firebase';
 import AllergenBadge from './AllergenBadge';
 
 export default function AddModal(props) {
-    // item's reference in Cloud Firestore DB.
-    const [itemRef, setItemRef] = useState('');
-    const [allergenItems, setAllergenItems] = useState([]);
+  // item's reference in Cloud Firestore DB.
+  const [itemRef, setItemRef] = useState('');
+  const [allergenItems, setAllergenItems] = useState([]);
 
-    const getAllergens = (value) => {
-      const properValue = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
-      if (!allergenItems.includes(properValue)) {
-        setAllergenItems([...allergenItems, properValue]);
-        console.log(allergenItems);
+  const getAllergens = (value) => {
+    const properValue = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+    if (!allergenItems.includes(properValue)) {
+      setAllergenItems([...allergenItems, properValue]);
+      console.log(allergenItems);
+    }
+  };
+
+  const removeAllergen = (value) => {
+    var filteredItems = [];
+
+    allergenItems.forEach((item) => {
+      if (!(item == value)) {
+        filteredItems.push(item);
       }
-    };
+    });
 
-    const removeAllergen = (value) => {
-      var filteredItems = [];
+    console.log(filteredItems)
+    setAllergenItems(filteredItems)
+  }
 
-      allergenItems.forEach((item) => {
-        if (!(item == value)) {
-          filteredItems.push(item);
-        }
-      });
-      
-      console.log(filteredItems)
-      setAllergenItems(filteredItems)
-    }
+  const closeSelf = () => {
+    setAllergenItems([]);
 
-    const closeSelf = () => {
-      setAllergenItems([]);
+    props.onClose();
+  }
 
-      props.onClose();
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
 
-      const form = e.target;
-      const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+    formJson.allergens = allergenItems;
+    pushData(formJson);
+    console.log(formJson)
 
-      const formJson = Object.fromEntries(formData.entries());
-      formJson.allergens = allergenItems;
-      pushData(formJson);
-      console.log(formJson)
+    props.onClose();
 
-      props.onClose();
+    setAllergenItems([]);
+    e.target.reset();
+  }
 
-      setAllergenItems([]);
-      e.target.reset();
-    }
+  return (
+    <div className={`modal fade ${props.showModal ? 'show d-block' : ''}`} tabIndex="-1">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Add Item</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={closeSelf}></button>
+          </div>
+          <div className="modal-body">
+            <p>Modal body text goes here.</p>
 
-    return (
-        <div className={`modal fade ${props.showModal ? 'show d-block' : ''}`} tabIndex="-1">
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Add Item</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={closeSelf}></button>
+            <form onSubmit={handleSubmit}>
+              <div className="form-floating mb-3">
+                <input type="text" className="form-control" name="name" placeholder={props.itemName} required />
+                <label htmlFor="name">Item Name</label>
               </div>
-              <div className="modal-body">
-                <p>Modal body text goes here.</p>
 
-                <form onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col">
                   <div className="form-floating mb-3">
-                    <input type="text" className="form-control" name="name" placeholder={props.itemName} required />
-                    <label htmlFor="name">Item Name</label>
+                    <input type="text" className="form-control" name="dailyLimit" placeholder={props.dailyLimit} required />
+                    <label htmlFor="dailyLimit">Limit Per Day</label>
                   </div>
-
-                  <div className="row">
-                    <div className="col">
-                      <div className="form-floating mb-3">
-                        <input type="text" className="form-control" name="dailyLimit" placeholder={props.dailyLimit} required />
-                        <label htmlFor="dailyLimit">Limit Per Day</label>
-                      </div>
-                    </div>
-                    <div className="col">
-                      <div className="form-floating mb-3">
-                        <input type="text" className="form-control" name="personalLimit" placeholder={props.personLimit} required />
-                        <label htmlFor="personalLimit">Limit Per Person (Optional)</label>
-                      </div>
-                    </div>
+                </div>
+                <div className="col">
+                  <div className="form-floating mb-3">
+                    <input type="text" className="form-control" name="personalLimit" placeholder={props.personLimit} required />
+                    <label htmlFor="personalLimit">Limit Per Person (Optional)</label>
                   </div>
-
-                  <div className="row">
-                    <div className="col">
-                      <div className="form-floating mb-3">
-                        <input type="number" className="form-control" name="stock" placeholder="" required />
-                        <label htmlFor="stock">Stock</label>
-                      </div>
-                    </div>
-                    <div className="col">
-                      <div className="form-check form-check-inline">
-                        <label className="form-check-label" htmlFor="inStock">In stock</label>
-                        <input className="form-check-input" type="checkbox" name="inStock" defaultChecked={true} />
-                      </div>
-                    </div>
-                  </div>
-
-                  <InputMultiple onValueChange={getAllergens}></InputMultiple>
-
-                  {allergenItems.map((allergen, index) => (
-                    <AllergenBadge key={index} label={allergen} callback={removeAllergen} />
-                  ))}
-
-                  <hr />
-                  
-                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={closeSelf}>Cancel</button>
-                  <button type='submit' className="btn btn-primary">Save changes</button>
-                </form>
-                  
                 </div>
               </div>
-              
-            </div>
+
+              <div className="row">
+                <div className="col">
+                  <div className="form-floating mb-3">
+                    <input type="number" className="form-control" name="stock" placeholder="" required />
+                    <label htmlFor="stock">Stock</label>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="form-check form-check-inline">
+                    <label className="form-check-label" htmlFor="inStock">In stock</label>
+                    <input className="form-check-input" type="checkbox" name="inStock" defaultChecked={true} />
+                  </div>
+                </div>
+              </div>
+
+              <InputMultiple onValueChange={getAllergens}></InputMultiple>
+
+              {allergenItems.map((allergen, index) => (
+                <AllergenBadge key={index} label={allergen} callback={removeAllergen} />
+              ))}
+
+              <hr />
+
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={closeSelf}>Cancel</button>
+              <button type='submit' className="btn btn-primary">Save changes</button>
+            </form>
+
           </div>
-    );
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 function pushData(data) {
   const docRef = addDoc(collection(db, "foodItems"), {
     allergens: data.allergens,
-    inStock: data.inStock,
+    inStock: data.inStock ? true : false,
     limitPerDay: data.dailyLimit,
     limitPerPerson: data.personalLimit,
     name: data.name,
@@ -150,7 +150,7 @@ function InputMultiple(props) {
       props.onValueChange(inputValue);
       setInputValue('');
     }
-    
+
   }
 
   return (
